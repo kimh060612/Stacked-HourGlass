565e9ddb0f1b2080e92a105213e90babd6a83f9c
kimh060612 <kimh060612@naver.com>
2020-07-18 21:38:02 +0900

    .

--- model/layers.py
+++ model/layers.py
@@ -1,5 +1,12 @@
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow import keras as tfk
 
 
+class ResidualLayer(tfk.layers.Layer):
+    def __init__(self):
+        super(ResidualLayer, self).__init__(name="Residual")
 
+
+    def call(self, input):
+        
+        pass
